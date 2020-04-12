d9cbf5fd41a049cf015922108f708fa51b4dbabd
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-04-12 17:54:36 +0000

    Added a spinning loader

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 //console.log(choices);
 
 let currentQuestion = {};
@@ -45,6 +47,8 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 }
 
 getNewQuestion = () => {
