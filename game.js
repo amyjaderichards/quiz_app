a0b9908e625991581ab723d6be7baba5d336a8d2
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-04-12 17:15:06 +0000

    Moved questions to local JSON file

--- game.js
+++ game.js
@@ -11,33 +11,17 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript??",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [];
+
+fetch("questions.json").then(res => {
+    return res.json();
+}).then(loadedQuestions => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+}).catch(err => {
+    console.error(err);
+})
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
@@ -61,7 +45,7 @@ getNewQuestion = () => {
 
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     // Update the progress bar
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)*100}%`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
@@ -86,7 +70,7 @@ choices.forEach(choice => {
 
         const classToApply = (selectedAnswer == currentQuestion.answer) ? 'correct' : 'incorrect';
 
-        if(classToApply === 'correct') {
+        if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
 
@@ -104,4 +88,3 @@ incrementScore = num => {
     scoreText.innerText = score;
 };
 
-startGame();
